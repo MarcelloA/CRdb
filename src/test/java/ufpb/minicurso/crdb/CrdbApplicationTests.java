133ed957e1384d5e7f2b798bd0b00821d6aaeff5
MarcelloA <marcelloassis@cc.ci.ufpb.br>
2020-08-08 02:49:48 -0300

    Initial commit

---
+++ src/test/java/ufpb/minicurso/crdb/CrdbApplicationTests.java
@@ -0,0 +1,13 @@
+package ufpb.minicurso.crdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
